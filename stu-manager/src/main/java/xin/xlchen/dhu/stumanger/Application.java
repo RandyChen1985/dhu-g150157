d6563094f753c625bbdd3c65a3f31bb06de9dadd
randy <cexlong@gmail.com>
2016-11-22 17:06:10 +0800

    变更: 完成freemarker集成&读取外部application配置;

--- stu-manager/src/main/java/xin/xlchen/dhu/stumanger/Application.java
+++ stu-manager/src/main/java/xin/xlchen/dhu/stumanger/Application.java
@@ -1,5 +1,7 @@
 package xin.xlchen.dhu.stumanger;
 
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.log4j.Logger;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -13,8 +15,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
 
 @EnableAutoConfiguration
 @SpringBootApplication
@@ -55,5 +55,5 @@ public class Application {
         SpringApplication.run(Application.class, args);
         logger.info("SpringBoot Start Success");
     }
-
+    
 }
--- stu-manager/src/main/java/xin/xlchen/dhu/stumanger/controller/HelloController.java
+++ stu-manager/src/main/java/xin/xlchen/dhu/stumanger/controller/HelloController.java
@@ -1,12 +1,23 @@
 package xin.xlchen.dhu.stumanger.controller;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@ComponentScan
 public class HelloController {
+	
+	@Value("${application.xiaolong:Hello World}")
+	private String message = "Hello World";
 
     @RequestMapping("/hello")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
@@ -14,4 +25,11 @@ public class HelloController {
         return "hello";
     }
     
+    
+    @RequestMapping(value={"/test"})
+	public String index(ModelMap model, HttpServletRequest request) {
+    	 model.put("time", new Date());
+         model.put("message", this.message);
+		return "aaa";
+	}
 }
