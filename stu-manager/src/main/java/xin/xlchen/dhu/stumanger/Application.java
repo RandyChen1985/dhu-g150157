b99c698cb5cdb39b855fa0df898b9afebfb6513b
randy <cexlong@gmail.com>
2016-11-22 14:51:04 +0800

    变更:完成了package模块;

--- stu-manager/src/main/java/xin/xlchen/dhu/stumanger/Application.java
+++ stu-manager/src/main/java/xin/xlchen/dhu/stumanger/Application.java
@@ -19,7 +19,7 @@ import javax.sql.DataSource;
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
-@MapperScan("cn.no7player.mapper")
+@MapperScan("xin.xlchen.dhu.stumanger.mapper")
 public class Application {
     private static Logger logger = Logger.getLogger(Application.class);
 
